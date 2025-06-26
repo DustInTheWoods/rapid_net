f5dae90cbc2af12737d49cc87654dc7495923f89
DustIn <117292644+DustInTheWoods@users.noreply.github.com>
2025-06-26 07:43:18 +0200

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-pub mod server;
-pub mod config;
 pub mod client;
+pub mod config;
 pub mod message;
 pub mod rapid_log;
+pub mod server;
 
 /* Re-exports */
-pub use client::{OutboundClient as RapidClient, InboundClient};
-pub use server::RapidServer;
+pub use client::{InboundClient, OutboundClient as RapidClient};
 pub use config::{RapidClientConfig, RapidServerConfig};
 pub use message::{ClientEvent, ServerEvent};
+pub use server::RapidServer;
